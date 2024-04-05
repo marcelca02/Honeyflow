083e8443b3e4c344d0476100d458d4d5426fc783
pablo.franco.c <pablo.franco.c@estudiantat.upc.edu>
2024-04-05 13:14:10 +0200

    deploy and stop honeypot (docker) added. [+] Py File: run_docker_cowrie.py + more templates + modif routes.py

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,12 +1,50 @@
-from flask import Blueprint, render_template
+import subprocess
+#import docker
+from flask import Blueprint, render_template, request
 
 
 routes = Blueprint('routes', __name__)
+
+# Variable global para saber si el docker del honeypot esta corriendo o no
+docker_running = False
+
 
 @routes.route('/')
 def home():
     return render_template('index.html')
 
+
 @routes.route('/deploy_honeypot')
 def deploy_honeypot():
     return render_template('deploy_honeypot.html')
+
+
+@routes.route('/ejecutar_docker', methods=['POST'])
+def ejecutar_docker():
+    global docker_running # indicamos que se usa la variable global
+    proceso = subprocess.run(['python3', 'run_docker_cowrie.py'])
+    if proceso.returncode == 0:
+        docker_running = True
+        return render_template('resultado_honeypot.html', deploy=True, exito=True)
+    else:
+        docker_running = False # no haria falta si ya es False por defecto
+        return render_template('resultado_honeypot.html', deploy=True, exito=False)
+
+
+@routes.route('/stop_honeypot')
+def stop_honeypot():
+    return render_template('stop_honeypot.html', docker_running=docker_running)
+
+
+@routes.route('/stop_docker', methods=['POST'])
+def stop_docker():
+    global docker_running
+    # de momento solo habra un resultado solo uno corriendo
+    proceso = subprocess.run(['docker', 'ps', '-a', '--filter', 'ancestor=honeypot1:v1','--format', '{{.ID}}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    container_id = proceso.stdout.decode().strip()
+    proceso2 = subprocess.run(['docker', 'stop', container_id])
+    if proceso2.returncode == 0:
+        docker_running = False
+        return render_template('resultado_honeypot.html', deploy=False, exito=True)
+    else:
+        return render_template('resultado_honeypot.html', deploy=False, exito=False)
---
+++ server/app/run_docker_cowrie.py
@@ -0,0 +1,46 @@
+import subprocess
+import os
+
+def crear_docker_network():
+    # Comprobar si la red de Docker existe
+    proceso = subprocess.Popen(['docker', 'network', 'inspect', 'honeypot-network'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    salida, _ = proceso.communicate()
+
+    if proceso.returncode != 0:  # La red no existe, crearla
+        subprocess.run(['docker', 'network', 'create', '-d', 'bridge', 'honeypot-network'])
+        print("Red de Docker creada exitosamente.")
+    else:
+        print("La red de Docker ya existe.")
+
+def construir_docker_image():
+    # Cambiar al directorio donde se encuentra el Dockerfile
+    ruta_dockerfile = "/home/pablofc18/Desktop/Honeyflow/server/app/machines"
+    os.chdir(ruta_dockerfile)
+
+    # Construir la imagen si no existe
+    proceso = subprocess.Popen(['docker', 'images', '-q', 'honeypot1:v1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    salida, _ = proceso.communicate()
+
+    if not salida:  # La imagen no existe, construirla
+        subprocess.run(['docker', 'build', '-t', 'honeypot1:v1', '.'])
+        print("Imagen de Docker construida exitosamente.")
+    else:
+        print("La imagen de Docker ya existe.")
+
+def ejecutar_o_iniciar_docker_container():
+    # Comprobar si el contenedor ya existe
+    proceso = subprocess.run(['docker', 'ps', '-a', '--filter', 'ancestor=honeypot1:v1','--format', '{{.ID}}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if proceso.stdout:  # El contenedor ya existe, iniciar
+        contenedor_id = proceso.stdout.decode().strip() # Obtener el ID del contenedor
+        subprocess.run(['docker', 'start', contenedor_id])
+        print("Contenedor Docker iniciado.")
+    else:  # El contenedor no existe, ejecutar
+        subprocess.run(['docker', 'run', '--network=honeypot-network', 'honeypot1:v1', '--name', 'cowrie'])
+        print("Contenedor Docker ejecutado.")
+
+# Llamar a las funciones en orden
+crear_docker_network()
+construir_docker_image()
+ejecutar_o_iniciar_docker_container()
+
