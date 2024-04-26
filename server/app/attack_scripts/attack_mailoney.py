1e52200e8e114107d5e7860f7808ebefa50039ba
pablo.franco.c <pablo.franco.c@estudiantat.upc.edu>
2024-04-26 13:03:18 +0200

    attacks 3 honeypots

--- server/app/attack_scripts/attack_mailoney.py
+++ server/app/attack_scripts/attack_mailoney.py
@@ -3,9 +3,7 @@
 import smtplib
 from email.mime.text import MIMEText
 
-def enviar_correo_malicioso(destinatario, asunto, cuerpo, servidor_smtp, puerto_smtp):
-	remitente = "correo@falso.com"  # Dirección de correo electrónico falsa
-
+def enviar_correo_malicioso(remitente, destinatario, asunto, cuerpo, servidor_smtp, puerto_smtp):
 	# Crear el mensaje de correo electrónico
 	mensaje = MIMEText(cuerpo)
 	mensaje['From'] = remitente
@@ -13,7 +11,7 @@ def enviar_correo_malicioso(destinatario, asunto, cuerpo, servidor_smtp, puerto_
 	mensaje['Subject'] = asunto
 
 	try:
-		# Conectar al servidor SMTP y enviar el correo electrónico
+		# (DATA not implemented)
 		servidor = smtplib.SMTP(servidor_smtp, puerto_smtp)
 		servidor.sendmail(remitente, [destinatario], mensaje.as_string())
 		servidor.quit()
@@ -23,15 +21,74 @@ def enviar_correo_malicioso(destinatario, asunto, cuerpo, servidor_smtp, puerto_
 
 
 
-def main():
+import telnetlib3, traceback
+
+async def enviar_correo_telnet(asunto, remitente, destinatario, cuerpo, servidor_smtp):
+    try:
+	# establecer conexion con telnet
+        reader, writer = await telnetlib3.open_connection(servidor_smtp, 25)
+
+	# read until -> esperar para asegurar que el servidor haya enviado respuesta ok
+        await reader.readuntil(b"220")
+        writer.write("HELO example.com\r\n")
+        await writer.drain()
+        await reader.readuntil(b"250")
+
+	# remitente
+        writer.write(f"MAIL FROM: <{remitente}>\r\n")
+        await writer.drain()
+        await reader.readuntil(b"250")
+
+	# destinatario
+        writer.write(f"RCPT TO: <{destinatario}>\r\n")
+        await writer.drain()
+        await reader.readuntil(b"250")
+
+	# cuerpo
+        writer.write(f"DATA + {cuerpo}\r\n")
+        await writer.drain()
+        #await reader.readuntil(b"354")
+
+	# enviar todo
+        # message = f"Subject: {asunto}\r\n"
+        # message += f"From: {remitente}\r\n"
+        # message += f"To: {destinatario}\r\n"
+        # message += "\r\n"  # Separator between headers and body
+        # message += cuerpo + "\r\n"
+        # message += ".\r\n"  # End-of-message signal
+        # writer.write(message)
+        # await writer.drain()
+        # await reader.readuntil(b"250")
+
+	# quit command
+        #writer.write(b"QUIT\r\n")
+        #await writer.drain()
+        #await reader.readuntil(b"221")
+
+	# close connection
+        writer.close()
+        #await writer.waitclosed()
+
+        print("[+] Correo electrónico malicioso enviado con éxito.")
+    except Exception as e:
+        traceback.print_exc()
+        print(f"Error al enviar el correo electrónico: {e}")
+
+
+
+async def main():
 	destinatario = "victima@gmail.com"
 	asunto = "Su cuenta ha sido comprometida"
-	cuerpo = "Estimado usuario,\n\nSu cuenta ha sido comprometida. Por favor, haga clic en el siguiente enlace para recuperar el acceso a su cuenta: http://sitio.malicioso.com/recuperar\n\nAtentamente,\nEl equipo de soporte"
-	servidor_smtp = "172.18.0.2"
-	puerto_smtp = 25  # Puerto SMTP utilizado por Mailoney
+	cuerpo = "Estimado usuario, Su cuenta ha sido comprometida. Por favor, haga clic en el siguiente enlace para recuperar el acceso a su cuenta: http://sitio.malicioso.com/recuperar. Atentamente, El equipo de soporte"
+	#servidor_smtp = "172.18.0.2"
+	puerto_smtp = 25
 
-	enviar_correo_malicioso(destinatario, asunto, cuerpo, servidor_smtp, puerto_smtp)
+	remitente = "correofalso@gmail.com"
+	#enviar_correo_malicioso(remitente, destinatario, asunto, cuerpo, "172.18.0.3", puerto_smtp)
+
+	await enviar_correo_telnet(asunto, remitente, destinatario, cuerpo, "172.18.0.3")
+
+import asyncio
 
 if __name__ == "__main__":
-    main()
-
+    asyncio.run(main())
