08eb681d2a05f9fb4af361a283e0491db2dcdfdb
marcelca02 <marcel.calleja@estudiantat.upc.edu>
2024-05-08 12:58:59 +0200

    funciones para crear pods honeypots

--- server/app/kubernetes.py
+++ server/app/kubernetes.py
@@ -2,31 +2,31 @@ from kubernetes import client, config
 import subprocess
 from app.config import COWRIE_IMAGE
 
-def init_k8s():
-    config.load_kube_config()
-    v1 = client.CoreV1Api()
-    return v1
+# def init_k8s():
+#     config.load_kube_config()
+#     v1 = client.CoreV1Api()
+#     return v1
 
 def create_cowrie_pod(v1, namespace='default'):
-    # # Comand line version
-    service_name = 'cowrie-service'
-    port = '8081'
-
     subprocess.run(['kubectl', 'apply', '-f', 'app/machines/cowrie/pod_cowrie.yaml'])
-    subprocess.run(['kubectl', 'expose', 'pod', 'cowrie', '--type=NodePort', '--port=' + port, '--target-port=22', '--name=' + service_name, '--external-ip=0.0.0.0'])
+    subprocess.run(['kubectl', 'apply', '-f', 'app/machines/cowrie/service_cowrie.yaml'])
 
 def create_mailoney_pod():
-    # # Comand line version
-    service_name = 'mailoney-service'
-    port = '8082'
-
     subprocess.run(['kubectl', 'apply', '-f', 'app/machines/mailoney/pod_mailoney.yaml'])
-    # Esto ahora mismo no funciona
-    subprocess.run(['kubectl', 'expose', 'pod', 'mailoney', '--type=NodePort', '--port=' + port, '--target-port=25','--name=' + service_name, '--external-ip=0.0.0.0'])
+    subprocess.run(['kubectl', 'apply', '-f', 'app/machines/cowrie/service_mailoney.yaml'])
+
+def create_heralding_pod():
+    subprocess.run(['kubectl', 'apply', '-f', 'app/machines/heralding/pod_heralding.yaml'])
+    subprocess.run(['kubectl', 'apply', '-f', 'app/machines/heralding/service_heralding.yaml'])
+
+def delete_pods():
+    subprocess.run(['kubectl', 'delete', 'pod', 'cowrie'])
+    subprocess.run(['kubectl', 'delete', 'pod', 'mailoney'])
+    subprocess.run(['kubectl', 'delete', 'pod', 'heralding'])
 
 
-def get_pods(v1, namespace='default'):
-    return v1.list_namespaced_pod(namespace)
+# def get_pods(v1, namespace='default'):
+#     return v1.list_namespaced_pod(namespace)
 
 def delete_pod(name):
     subprocess.run(['kubectl', 'delete', 'pod', name])
